976ddad8f8f0289c1f54b4b95a04290a4150949c
gabriellau- <40619564+gabriel-lau@users.noreply.github.com>
2023-05-25 23:19:43 +0800

    Update gui.py
    drag and drop images working, need to save images now and add comments. Also need to confirm file types to use

--- gui.py
+++ gui.py
@@ -1,15 +1,20 @@
 import typing
-from PyQt6 import QtCore
-from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QGridLayout, QVBoxLayout, QPushButton, QLabel, QPlainTextEdit, QComboBox, QMessageBox, QFileDialog
+from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QGridLayout, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QPlainTextEdit, QComboBox, QMessageBox, QFileDialog
+from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt
 import sys
 
-
+# Widget for image/document/audio drag and drop
 class DNDWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.setAcceptDrops(True)
         self.filePath = ""
+        self.mainLayout = QHBoxLayout()
+        self.dndInfoWidget = QWidget()
+        self.imageWidget = QLabel()
+        
+        self.imageWidget.setHidden(True)
         
         selectFilePushButton = QPushButton("Click here to select files")
         selectFilePushButton.clicked.connect(self.return_clicked)
@@ -20,8 +25,13 @@ class DNDWidget(QWidget):
         dndInfoVerticalLayout.addWidget(QLabel("or"), alignment=Qt.AlignmentFlag.AlignCenter)
         dndInfoVerticalLayout.addWidget(selectFilePushButton)
         dndInfoVerticalLayout.addStretch()
+        self.dndInfoWidget.setLayout(dndInfoVerticalLayout)
         
-        self.setLayout(dndInfoVerticalLayout)
+        self.mainLayout.addWidget(self.dndInfoWidget)
+        self.mainLayout.addWidget(self.imageWidget)
+        
+        
+        self.setLayout(self.mainLayout)
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
@@ -32,19 +42,28 @@ class DNDWidget(QWidget):
     def dropEvent(self, event):
         files = [u.toLocalFile() for u in event.mimeData().urls()]
         for f in files:
-            self.filePath = f
+            self.setFilePath(f)
     
     def return_clicked(self):
         dlg = QFileDialog()
         dlg.setFileMode(QFileDialog.FileMode.ExistingFiles)
         dlg.setNameFilter("Images (*.png  *.jpg);;Text (*.txt);;Audio (*.mp3)")
         if dlg.exec():
-            filenames = dlg.selectedFiles()
-            print(filenames)
+            self.setFilePath(dlg.selectedFiles()[0])
+    
+    def setFilePath(self, filePath):
+        print(filePath)
+        if filePath.endswith(".png") or filePath.endswith(".jpg"):
+            pixmap = QPixmap(filePath)
+            self.imageWidget.setPixmap(pixmap.scaled(720, 480, Qt.AspectRatioMode.KeepAspectRatio))
+            self.imageWidget.setHidden(False)
+            self.dndInfoWidget.setHidden(True)
+        self.filePath = filePath
     
     def getFilePath(self):
         return self.filePath
 
+# Widget for encrypting text
 class EncryptWidget(QWidget):
     def __init__(self):
         super().__init__()
@@ -66,6 +85,7 @@ class EncryptWidget(QWidget):
     def getBits(self):
         return self.comboBox.currentIndex()
 
+# Widget for decrypting text
 class DecryptWidget(QWidget):
     def __init__(self):
         super().__init__()
@@ -88,33 +108,47 @@ class DecryptWidget(QWidget):
     def getBits(self):
         return self.comboBox.currentIndex()
 
+# Main window
+# Contains all widgets above, buttons to encrypt/decrypt and save
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        
+        # BASIC WINDOW SETTINGS
         self.setWindowTitle("My App")
-        self.resize(720, 480)
+        self.resize(720, 720)
+        self.setFixedWidth(720)
         mainWidget = QWidget()
         
+        # LAYOUT
         gridLayout = QGridLayout()
         self.dndWidget = DNDWidget()
         gridLayout.addWidget(self.dndWidget, 0, 0, 1, 2)
         
+        # ENCRYPT WIDGET
         self.encryptWidget = EncryptWidget()
         gridLayout.addWidget(self.encryptWidget, 1, 0)
         
-        self.decryptWidget = DecryptWidget()
-        gridLayout.addWidget(self.decryptWidget, 1, 1)
-        
+        # ENCRYPT BUTTON
         encryptPushButton = QPushButton("Encrypt")
         gridLayout.addWidget(encryptPushButton, 2, 0)
         encryptPushButton.clicked.connect(self.encyptClicked)
         
+        # DECRYPT WIDGET
+        self.decryptWidget = DecryptWidget()
+        gridLayout.addWidget(self.decryptWidget, 1, 1)
+        
+        # DECRYPT BUTTONS
         decryptPushButton = QPushButton("Decrypt")
         gridLayout.addWidget(decryptPushButton, 2, 1)
         decryptPushButton.clicked.connect(self.decrypt_clicked)
         
-        # Set the central widget of the Window.
+        # SAVE BUTTON
+        savePushButton = QPushButton("Save")
+        gridLayout.addWidget(savePushButton, 3, 0, 1, 2)
+        savePushButton.clicked.connect(self.save_clicked)
+        
+        # FINAL WINDOW SETTINGS
         mainWidget.setLayout(gridLayout)
         self.setCentralWidget(mainWidget)
         
@@ -147,6 +181,21 @@ class MainWindow(QMainWindow):
             print(bits)
             #self.decryptWidget.setText(decrypt(filePath, bits))
             self.decryptWidget.setText(filePath)
+            
+    def save_clicked(self):
+        filePath = self.dndWidget.getFilePath()
+        if filePath == "":
+            dlg = QMessageBox(self)
+            dlg.setWindowTitle("Error")
+            dlg.setText("Please ensure that all fields are filled")
+            dlg.exec()
+        else:
+            dlg = QFileDialog()
+            dlg.setAcceptMode(QFileDialog.AcceptMode.AcceptSave)
+            dlg.saveFileContent(filePath)
+            if dlg.exec():
+                filenames = dlg.selectedFiles()
+                print(filenames)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
